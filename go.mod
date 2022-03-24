2d3a0c01c5c16b59e2152caa1da903aacbc7badf
Raja Genupula <raja.genupula@yahoo.com>
2022-03-24 09:40:30 +0530

    a webserver which reads input from html form and shows to the user

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rajagennu/p11-webserver-parsing-form
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+	}
+	fmt.Fprintf(w, "Hello")
+
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/form" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err : %v", err)
+		return
+	}
+
+	if r.Method != "POST" {
+
+		http.Error(w, "method not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Post Request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8000\n")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
